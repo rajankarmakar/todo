341e3afd722f03f3e5571e903d08340aa1a12c09
unknown <rajankarmakar@hotmail.comm>
2019-08-27 14:26:31 +0600

    Updates

---
+++ precache-manifest.60001a6047429957d3c6140d41a99ddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b5689949b8400759c7f38043dbd25e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c859634f657f3461bef",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c5337f056c9331c4630e",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "9c859634f657f3461bef",
+    "url": "/static/js/2.d5a34201.chunk.js"
+  },
+  {
+    "revision": "c5337f056c9331c4630e",
+    "url": "/static/js/main.3b21b196.chunk.js"
+  },
+  {
+    "revision": "41ce867691ed58af39db",
+    "url": "/static/js/runtime~main.42afcef2.js"
+  }
+]);
